d790569b4ee2e2b7b3466d7d69b5f57ca7aa81d3
bhumika <bhumikakaddugol3011@gmail.com>
2020-07-30 21:19:18 +0530

    second commit

--- Welcome.java
+++ Welcome.java
@@ -3,5 +3,10 @@ public class Welcome
 public static void main(String [] args)
 {
 System.out.println("welcome to jenkin");
+
+for(int i=0;i<10;i++)
+{
+System.out.println("welcome to jenkin");
+}
 }
 }
